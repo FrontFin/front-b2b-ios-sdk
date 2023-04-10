6ec7a85c5732bda03d0707564bef837a1dc160c8
Alexander Zemlyanukhin <alex.zeml@getfront.com>
2023-04-10 14:44:15 +0500

    FrontLinkSDK v 1.1.0 - added/modified BrokerConnectViewControllerDelegate protocol function for more customization and flexibility

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "FrontLinkSDK", url: "https://github.com/FrontFin/front-b2b-ios-sdk/releases/download/1.0.0/FrontLinkSDK.xcframework.1.0.zip", checksum: "aee8dc6401eec0cdfa338daf135cb8bb18a1c611dcd83871e93e514ad5752b0b")
+        .binaryTarget(name: "FrontLinkSDK", url: "https://github.com/FrontFin/front-b2b-ios-sdk/releases/download/1.1.0/FrontLinkSDK.xcframework.1.1.0.zip", checksum: "ce3785880c4f066b21f2022636d67fe04512d4d208340005327e64fba0ae609d")
     ]
 )
 
